bc2ad2546215887cf29c15198a690a03296f7e42
Martin Heide <martin.heide@gmx.net>
2023-08-29 21:33:53 +0200

    Usable database

---
+++ README.md
@@ -0,0 +1,44 @@
+# db.sh - Append-only Bash database
+
+Roughly based on an example in Martin Kleppmann's book [Designing Data-Intensive Applications](https://dataintensive.net/).
+
+## How does it work?
+
+Startup: The database file is "sourced" to evaluate all assignments from previous runs.
+
+Write operations: Write operations are specified in the form of a Bash variable assigment instruction.  
+This instruction is 1) executed with `eval` and 2) appended to the database file.
+
+Read operations: Read operations are specified simply as the variable name.  
+This variable is printed using `eval "printf '%s\n' ..."`.
+
+All variables are internally prefixed with `dbsh_`, to avoid overwriting predefined variables.
+
+
+Examples:
+
+```bash
+# Write command:
+my_var="\"That's a hell of a database\", he said."
+# ...is executed as:
+eval dbsh_my_var="\"That's a hell of a database\", he said."
+echo dbsh_my_var="\"That's a hell of a database\", he said." >> "$db_file"
+
+# Read command:
+my_var
+# ...is executed as:
+printf eval "printf '%s\n' \"$dbsh_myvar\""
+```
+
+## Is this safe?
+
+This code is using `eval`. Can this be safe?
+
+```bash
+./db.sh -c "safe=Yes \"No, of course not...\""
+./db.sh: line 62: No, of course not...: command not found
+```
+
+## Should I use it in production?
+
+If you currently use a Powershell script as your database, then it could be an improvement.
--- db.sh
+++ db.sh
@@ -1,47 +1,75 @@
 #!/bin/bash
 set -e
 
-db_file="$HOME/.config/db.sh.data"
-db_dir="$( dirname "$db_file" )"
-cmd_cli=
+echo2() {
+    echo "$@" >&2
+}
 
-if [ "$1" = "-c" ]; then
-    echo "COMMAND ARG: $2" >&2
-    cmd_cli="$2"
-    shift
-    shift
+db_file="$HOME/.config/db.sh.data"
+cmd_cli=
+verbose=0
+
+while [[ "$1" != "" ]]; do
+    if [ "$1" = "-c" ]; then
+        cmd_cli="$2"
+        shift
+        shift
+    elif [ "$1" = "-v" ]; then
+        verbose=1
+        shift
+    elif [ "$1" = "--db" ]; then
+        db_file="$2"
+        shift
+        shift
+    else
+        echo "ERROR: Unknown option '$1'"
+        exit 1
+    fi
+done
+
+db_dir="$( dirname "$db_file" )"
+echo2 "Reading persistent data from \"$db_file\"..."
+if [ -f "$db_file" ]; then
+    if [ "$verbose" = 1 ]; then
+        cat "$db_file" >&2
+    fi
+    . "$db_file"
+    echo2 "Done."
+else
+    echo2 "File not found. -> Starting with empty database."
+    mkdir -pv "$db_dir"
 fi
 
-if [ -f "$db_file" ]; then
-    echo "Reading persistent data..." >&2
-    cat "$db_file" >&2
-    . "$db_file"
-else
-    mkdir -pv "$db_dir"
+if [ "$cmd_cli" = "" ]; then
+    echo2
+    echo2 "QUICK REFERENCE:"
+    echo2 "HOW TO WRITE: var=value_no_spaces  OR  var=\"value\"  OR  var='value' (with Bash quoting rules)."
+    echo2 "HOW TO READ:  var"
+    echo2 "CLI WRITE:    ./db.sh -c \"var=\\\"hello world\\\"\""
+    echo2 "CLI READ:     ./db.sh -c \"var\""
 fi
 
 while true; do
     if [ "$cmd_cli" != "" ]; then
+        echo2
         cmd="$cmd_cli"
     else
-        echo -n "> " >&2
-        read -r cmd
+        echo2; echo2 "Command?"
+        read -er cmd
     fi
     if [ "$cmd" = "" ]; then
         continue
     fi
 
     if [[ "$cmd" =~ = ]]; then
-        echo "WRITE $cmd" >&2
+        echo2 "WRITE $cmd"
         # Save for current process:
-        eval "db_$cmd"
+        eval "dbsh_$cmd"
         # Save for next run:
-        echo "db_$cmd" >> "$db_file"
+        echo "dbsh_$cmd" >> "$db_file"
     else
-        echo "READ $cmd" >&2
-        # echo "READ $cmd" >&2
-        # echo eval "echo \"\$db_$cmd\""
-        eval "echo \"\$db_$cmd\""
+        echo2 "READ $cmd"
+        eval "printf '%s\n' \"\$dbsh_$cmd\""
     fi
     if [ "$cmd_cli" != "" ]; then
         exit
